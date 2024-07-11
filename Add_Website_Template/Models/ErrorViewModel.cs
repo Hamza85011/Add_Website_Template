eb3b92303093c1759e181591f5f288a9195a062e
hamza khan <hamza.khan@aircod.com>
2024-07-11 15:41:22 +0500

    Add project files.

---
+++ Add_Website_Template/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Add_Website_Template.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
